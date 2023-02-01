a3d2f8aae030b5219f15f85aab858a29af84ef8b
sarman-dasa <sarmand@zignuts.com>
2023-02-01 18:11:09 +0530

    fetch data from data base via model and print on browser using controller

--- Controller-Route-App/app/Http/Controllers/EmployeeProfileController.php
+++ Controller-Route-App/app/Http/Controllers/EmployeeProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeProfileController extends Controller
@@ -13,7 +14,7 @@ class EmployeeProfileController extends Controller
      */
     public function index()
     {
-        return "Index Function Call...";
+        return Employee::all();
     }
 
     /**
