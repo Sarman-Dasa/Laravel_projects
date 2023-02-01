ea9b3344e4c6932ea3d97a91d7fd8a854afc19c7
sarman-dasa <sarmand@zignuts.com>
2023-02-01 14:53:50 +0530

    add form tag and csrf token

--- Controller-Route-App/resources/views/user.blade.php
+++ Controller-Route-App/resources/views/user.blade.php
@@ -7,33 +7,36 @@
     <title>User Data</title>
 </head>
 <body>
-    <table border="2" align="center">
-        <tr>
-            <th colspan="2">User Data</th>
-        </tr>
-        <tr>
-            <td>User Name</td>
-            <td>
-                <input type="text" name="username" id="userID">
-            </td>
-        </tr>
-        <tr>
-            <td>User Email</td>
-            <td>
-                <input type="email" name="userEmail" id="userEmailID">
-            </td>
-        </tr>
-        <tr>
-            <td>Mobile No.</td>
-            <td>
-                <input type="tel" name="mobileNo" id="mobileID" pattern="^[0-9]+">
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center">
-                <input type="submit" value="submit" name="submit">
-            </td>   
-        </tr>
-    </table>
+    <form action="" method="post">
+        @csrf
+        <table border="2" align="center">
+            <tr>
+                <th colspan="2">User Data</th>
+            </tr>
+            <tr>
+                <td>User Name</td>
+                <td>
+                    <input type="text" name="username" id="userID">
+                </td>
+            </tr>
+            <tr>
+                <td>User Email</td>
+                <td>
+                    <input type="email" name="userEmail" id="userEmailID">
+                </td>
+            </tr>
+            <tr>
+                <td>Mobile No.</td>
+                <td>
+                    <input type="tel" name="mobileNo" id="mobileID" pattern="^[0-9]+">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center">
+                    <input type="submit" value="submit" name="submit">
+                </td>   
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
