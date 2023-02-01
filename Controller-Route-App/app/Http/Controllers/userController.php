26bc7d0008fc9f84cb803f6cbb4a3ec6d36f6e36
sarman-dasa <sarmand@zignuts.com>
2023-02-01 11:14:52 +0530

    create new laravel project add controller and route code

---
+++ Controller-Route-App/app/Http/Controllers/userController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    function index(){
+        return "Calling Controller...";
+    }
+
+    //Paramenter print
+
+    function getName($name)
+    {
+        return "My Name is: ".$name;
+    }
+}
