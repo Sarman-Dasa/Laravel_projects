34fb04575f39dcd0b96075a011515fdcdff04ee1
sarman-dasa <sarmand@zignuts.com>
2023-02-07 19:34:21 +0530

    add code form call controller

--- employeeCrud/routes/web.php
+++ employeeCrud/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\departmentController;
+use App\Http\Controllers\DeploymentController;
 use App\Http\Controllers\employeeController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\MechanicController;
+use App\Http\Controllers\OwnerController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,6 +33,20 @@ Route::get('employee/deletedData/restore/{id}',[employeeController::class,'resto
 Route::delete('employee/deletedData/delete/{id}',[employeeController::class,'permanentDelete'])->name('employee.permanentDelete');
 
 // Department Controller
-Route::resource('depaertment',departmentController::class);
+Route::resource('department',departmentController::class);
+
+Route::get('mechanic',[MechanicController::class,'addMechanic']);
+
+Route::get('car/{id}',[CarController::class,'addCar']);
+Route::get('ownwer/{id}',[OwnerController::class,'addOwner']);
+
+Route::get('showoner/{id}',[OwnerController::class,'showOwner']);
+
+Route::get('project',[ProjectController::class,'addProject']);
+
+Route::get('language/{id}',[LanguageController::class,'addLanguage']);
+Route::get('addDeployement/{id}',[DeploymentController::class,'addDeployement']);
+
+Route::get('showProjectDeployement/{id}',[DeploymentController::class,'showData']);
 
 
