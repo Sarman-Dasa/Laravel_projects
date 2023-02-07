7fc25ecb73dc43162e6929ce6289ac3e90b08535
sarman-dasa <sarmand@zignuts.com>
2023-02-07 19:34:01 +0530

    create table form has many through relationship

---
+++ employeeCrud/database/migrations/2023_02_07_124412_create_deployments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deployments', function (Blueprint $table) {
+            $table->id();
+            $table->string("work",20);
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+            $table->foreign('language_id')->references('id')->on('languages')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deployments');
+    }
+};
