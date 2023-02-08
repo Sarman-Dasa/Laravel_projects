912fc879914baf6f1fd18f4cd2503965962e0f99
sarman-dasa <sarmand@zignuts.com>
2023-02-08 16:59:40 +0530

    create table for one to one Polymorphic relationship

---
+++ employeeCrud/database/migrations/2023_02_08_094834_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string("email",50);
+            $table->string('phone',12);
+            $table->unsignedBigInteger("profileable_id");
+            $table->string("profileable_type");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
