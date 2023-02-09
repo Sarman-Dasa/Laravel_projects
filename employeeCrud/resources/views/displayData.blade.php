4ae7353fe5df9c3b4a9345fe17b80357db2ac5b6
sarman-dasa <sarmand@zignuts.com>
2023-02-09 19:31:32 +0530

    change code display  department id to department name

--- employeeCrud/resources/views/displayData.blade.php
+++ employeeCrud/resources/views/displayData.blade.php
@@ -7,17 +7,18 @@
             <table class="table table-striped table-hover ">
                 <thead>
                 <tr>
-                    <th colspan="11" class="text-center table-dark">Employee Data</th>
+                    <th colspan="12" class="text-center table-dark">Employee Data</th>
                 </tr>
                 <tr class="table-info">
+                    <th>Employee Id</th>
                         <th>Full Name</th>
                         <th>Email</th>
                         <th>Mobile Number</th>
                         <th>Department</th>
                         <th>Hiredate</th>
                         <th>Birthdate</th>
-                        <th>Salary</th>
                         <th>Gender</th>
+                        <th>Salary</th>
                         <th>Image</th>
                         <th colspan="2" class="text-center">Action</th>
                 </tr>
@@ -26,14 +27,15 @@
                     <tr>
                         @foreach ($data as $item)
                            <tr>
+                                <td>{{$item->id}}</td>
                                 <td>{{$item->employee_name}}</td>
                                 <td>{{$item->employee_email}}</td>
                                 <td>{{$item->employee_mobile_number}}</td>
-                                <td>{{$item->department_id}}</td>
+                                <td>{{$item['department']['department_name']}}</td>
                                 <td>{{$item->employee_hiredate}}</td>
                                 <td>{{$item->employee_birthdate}}</td>
                                 <td>{{$item->employee_gender}}</td>
-                                <td>{{$item->employee_salary}}</td>
+                                <td>{{number_format($item->employee_salary,2)}}</td>
                               <td><img src="{{asset($item->employee_image)}}" style="width: 50px;height:50px;" download></td>
                                 <td>
                                     <form action="{{route('employee.edit',$item->id)}}" method="POST">
