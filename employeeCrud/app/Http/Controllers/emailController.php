4aaebc7a943c9171562283b4f66d5dc86791a8cd
sarman-dasa <sarmand@zignuts.com>
2023-02-14 14:54:31 +0530

    email verify code, login code added

--- employeeCrud/app/Http/Controllers/emailController.php
+++ employeeCrud/app/Http/Controllers/emailController.php
@@ -8,10 +8,12 @@ use App\Models\User;
 use Exception;
 use GuzzleHttp\Psr7\Message;
 use App\Notifications\VeriFyEmail;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
-
 class emailController extends Controller
 {
     public function sendEmail()
@@ -41,25 +43,48 @@ class emailController extends Controller
             'password' =>'required|digits:8',
             'confirmPassword' =>'required|same:password'
         ]);
-        $request['email_verify_token']=Str::random(15);
+        $request['password'] = Hash::make($request->password);
+        $request['email_verify_token'] = Str::random(64);
         $user = User::create($request->all());
-        
+        $user['email_verify_token'];
         $user->notify(new VeriFyEmail($user));
-        
-        //send Mail To user
-        // try {
-        //     Mail::to($request->email)->send(new verifyEmail());
-        //     return "Thanks for signing up! Before getting started, could you verify \n
-        //     your email address by clicking on the\n
-        //     link we just emailed to you?";
-        // } catch (Exception $th) {
-        //     return "Some Error".$th;
-        // }
-       // return redirect()->route("");
+    }
+
+    public function verifyEmail($token)
+    {
+       // return $token ?? "Not Work";
+      
+            //session()->forget('token');
+            $user = User::where('email_verify_token',$token);
+           // return $user;
+            $user->update([
+                'email_verified_at'=>now(),
+                'status' => 1,
+                'email_verify_token' => '',    
+            ]);
+            //return Auth::user()->name;
+            return redirect()->route('user.login');  
     }
 
     public function login()
     {
         return view('emails.userLogin');
+    }
+
+    public function isValidUser(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect("employee");
+        } else {
+            return redirect('/login')->with('error', 'Invalid Email address or Password');
+        }
+    }
+
+    public function logOut()
+    {
+        session()->flush();
+        Auth::logout();
+        return redirect("/");
     }
 }
