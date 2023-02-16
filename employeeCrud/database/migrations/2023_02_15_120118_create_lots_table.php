f0f3024b75a36c398c546100d19e1a0e62d049ff
sarman-dasa <sarmand@zignuts.com>
2023-02-16 19:37:02 +0530

    initial commit

---
+++ employeeCrud/database/migrations/2023_02_15_120118_create_lots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lots', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->decimal('price');
+            $table->integer('total') ;
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lots');
+    }
+};
