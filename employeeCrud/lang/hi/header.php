64e25aca5cafd0d038d6a1dc8ba2b5a2dab484e8
sarman-dasa <sarmand@zignuts.com>
2023-02-17 17:02:48 +0530

    create Localization for hindi

---
+++ employeeCrud/lang/hi/header.php
@@ -0,0 +1,12 @@
+<?php
+    return[
+        'home'         => 'घर',
+        'addEmployee'  => 'कर्मचारी जोड़ें',
+        'restoreData'  => 'डेटा पुनः स्थापित करें',
+        'showData'     => 'डेटा दिखाएं',
+        'addassignment'=> 'असाइनमेंट जोड़ें',
+        'showUser'     => 'उपयोगकर्ता दिखाएं',
+        'welcome'      => 'स्वागत है, :name',
+        'logout'       => 'लॉग आउट'
+    ]
+?>
