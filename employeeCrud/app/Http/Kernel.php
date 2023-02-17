811740de4987d82df83812c8ed57b4f9a4042d41
sarman-dasa <sarmand@zignuts.com>
2023-02-17 16:59:30 +0530

    register middleware

--- employeeCrud/app/Http/Kernel.php
+++ employeeCrud/app/Http/Kernel.php
@@ -69,5 +69,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'mwcheckAge' => \App\Http\Middleware\checkAge::class,
+        'validUser' => \App\Http\Middleware\ValidUser::class,
     ];
 }
