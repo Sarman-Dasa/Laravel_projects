a2b3daa3497c7243a39cdcc05eeb564a1021e523
sarman-dasa <sarmand@zignuts.com>
2023-02-20 19:26:39 +0530

    create api route to handle api request and response

--- passport-app/routes/api.php
+++ passport-app/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\UserAuthController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[UserAuthController::class,'register']);
+Route::post('/login',[UserAuthController::class,'login']);
+
+Route::apiResource('/employee',EmployeeController::class)->middleware('auth:api');
