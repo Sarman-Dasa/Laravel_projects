f098776f7271b97c6cf74a7c89047c0fb1d6d330
sarman-dasa <sarmand@zignuts.com>
2023-02-21 15:24:00 +0530

    create api route : register,login and apiResource route

--- sanctum-app/routes/api.php
+++ sanctum-app/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[AuthController::class,'registerUser']);
+Route::post('login',[AuthController::class,'login']);
+
+Route::apiResource('user',AuthController::class)->middleware('auth:sanctum');
